ea75bf38be64401d0c571fc817bf07f3a097ac31
Ming Li <leem7899@163.com>
2024-11-26 21:19:14 +0800

    todo

--- fastapis/app/main.py
+++ fastapis/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.openapi.docs import get_swagger_ui_html
-from routers import meta
+from routers import company, meta, user, vessel
 
 app = FastAPI(docs_url=None, redoc_url=None)
 
@@ -17,3 +17,10 @@ async def custom_swagger_ui_html():
 
 
 app.include_router(meta.api, prefix="/meta", tags=["元数据"])
+app.include_router(
+    vessel.api, prefix="/vessel", tags=["船舶"]
+)  # TODO: 1. 导入vessel模块
+app.include_router(
+    company.api, prefix="/company", tags=["公司"]
+)  # TODO: 2. 导入company模块
+app.include_router(user.api, prefix="/user", tags=["用户"])  # TODO: 3. 导入user模块
--- layer.md
+++ layer.md
@@ -1,5 +1,21 @@
+## 横向纬度
 DI Dependency Injection 依赖注入
 ORM Object Relationship Mapping 数据库操作 Select * from table -> Method
 1. router - API 定义
 2. service - 业务逻辑
 3. sqlmodal - 数据库操作 
+
+# 纵向纬度
+1. URI - URL 资源
+    1.1. GET - 查询
+    1.2. POST - 新增
+    1.3. PUT - 更新
+    1.4. DELETE - 删除
+    1.5. List - 列表
+
+# 前后端分离
+1. 前端 - React
+2. 后端 - Fastapi
+
+# 优势
+1. 前后端解耦 - 灵活，好维护
