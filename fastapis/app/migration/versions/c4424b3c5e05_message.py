0f662dc570b8bad783b0fe929504ad3f9a39a623
Jayzhuovo <2359492386@qq.com>
2025-01-17 17:25:17 +0800

    2

--- fastapis/app/migration/versions/c4424b3c5e05_message.py
+++ fastapis/app/migration/versions/c4424b3c5e05_message.py
@@ -5,15 +5,12 @@ Revises: 2012288dffc4
 Create Date: 2025-01-16 16:30:55.689253
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
-revision: str = 'c4424b3c5e05'
-down_revision: Union[str, None] = '2012288dffc4'
+revision: str = "c4424b3c5e05"
+down_revision: Union[str, None] = "2012288dffc4"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
